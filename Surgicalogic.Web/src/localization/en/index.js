0ad2e14f9dc608eed32469908acbe1abf82c3347
Hasan Çolak <hasan.colak@smartiks.com.tr>
2018-06-11 11:08:36 +0300

    Comman file added for globalization

--- Surgicalogic.Web/src/localization/en/index.js
+++ Surgicalogic.Web/src/localization/en/index.js
@@ -5,8 +5,10 @@
  * An example language file.
  */
 
+import * as comman from './comman.json'; 
 import * as equipments from './equipments.json';
 
 export default {
+    comman,
     equipments
 };
