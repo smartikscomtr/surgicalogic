3a0124358258eea1085d89f3a2640c8a15eb01f6
gurkank <gurkank@GURKANKESEBIR>
2018-06-20 16:00:20 +0300

    Improvements

---
+++ Surgicalogic.ORTools/Model/SurgeryPlan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surgicalogic.ORTools.Model
+{
+    public class SurgeryPlan
+    {
+        public int RoomCount { get; set; }
+        public int RoomsPeriod { get; set; }
+        public int MaximumPeriod { get; set; }
+        public int StartingHour { get; set; }
+        public int StartingMinute { get; set; }
+        public int PeriodInMinutes { get; set; }
+        public List<Operation> Operations { get; set; }
+    }
+}
