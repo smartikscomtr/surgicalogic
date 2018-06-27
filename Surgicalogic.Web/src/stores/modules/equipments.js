d01eebc04d1f2ae34f6d65a20a3e7a128ec5deed
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2018-06-27 15:57:51 +0300

    Equipment page controller and model fixes.

--- Surgicalogic.Web/src/stores/modules/equipments.js
+++ Surgicalogic.Web/src/stores/modules/equipments.js
@@ -5,7 +5,10 @@ const equipmentModule = {
     mutations: {
         setEquipments(state, equipments) {
             state.equipments = equipments;
-        },
+      },
+      insertEquipment(state, { item }) {
+        state.equipments.push(item);
+      },
         updateEquipment(state, payload){
           state.equipments.payload = payload;
         }
@@ -17,7 +20,19 @@ const equipmentModule = {
               .then(response => {
                 context.commit('setEquipments', response.data.result) // set the Equipments in the store
           })
-        },
+      },
+      insertEquipment(context, payload) {
+
+        axios.post('http://localhost:6632/Equipment/InsertEquipment', payload)
+          .then(response => {
+            if (response.statusText == 'OK') {
+              payload.id = response.data;
+
+              context.commit('insertEquipment', { item: payload }) // insert the Equipments in the store
+            }
+          })
+
+      },
         updateEquipment(context, payload){
           axios.delete('http://localhost:6632/Equipment/UpdateEquipment',{ equipment : payload })
               .then(response => {
