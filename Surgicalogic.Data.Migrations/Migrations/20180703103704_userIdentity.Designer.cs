c925ec97f52db7c499f7ff7ba48ece8ce9f07a4f
gurkank <gurkank@GURKANKESEBIR>
2018-07-03 13:43:53 +0300

    identity created.

--- Surgicalogic.Data.Migrations/Migrations/20180703103704_userIdentity.Designer.cs
+++ Surgicalogic.Data.Migrations/Migrations/20180703103704_userIdentity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Surgicalogic.Data.DbContexts;
 
 namespace Surgicalogic.Data.Migrations.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180703103704_userIdentity")]
+    partial class userIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Surgicalogic.Data/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Surgicalogic.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surgicalogic.Data.Entities
+{
+    public class User : IdentityUser
+    {
+    }
+}
