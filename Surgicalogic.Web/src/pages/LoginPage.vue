7b858f3d21884dc23d0c775d15b02c5fb7a3c7d6
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2018-07-11 09:31:31 +0300

    Login page düzenlendi

--- Surgicalogic.Web/src/pages/LoginPage.vue
+++ Surgicalogic.Web/src/pages/LoginPage.vue
@@ -5,28 +5,53 @@
         <v-flex xs12 sm8 md4>
           <v-card class="elevation-12">
             <v-toolbar dark color="primary">
-              <v-toolbar-title>Login form</v-toolbar-title>
+              <v-toolbar-title>
+                Login form
+              </v-toolbar-title>
+
               <v-spacer></v-spacer>
             </v-toolbar>
+
             <v-card-text>
               <v-form>
-                <v-text-field v-model="email" prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
-                <v-text-field v-model="password" prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+                <v-text-field v-model="email"
+                              prepend-icon="person"
+                              name="login"
+                              label="Login"
+                              type="text">
+                </v-text-field>
+
+                <v-text-field v-model="password"
+                              prepend-icon="lock"
+                              name="password"
+                              label="Password"
+                              id="password"
+                              type="password">
+                </v-text-field>
               </v-form>
             </v-card-text>
+
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn class="btnSave btn--flat" @click="login()">Login</v-btn>
+
+              <v-btn class="btnSave btn--flat"
+                     @click="login()">
+                Login
+              </v-btn>
             </v-card-actions>
+
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn class="btnSave btn--flat" @click="logout()">Logout</v-btn>
+
+              <v-btn class="btnSave btn--flat"
+                     @click="logout()">
+                Logout
+              </v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
       </v-layout>
     </v-container>
-
   </v-content>
 </template>
 
@@ -60,7 +85,7 @@ export default {
     const vm = this;
 
      if (vm.auth.isAuthenticated()) {
-      this.$router.push("DashboardPage");
+      vm.$router.push("DashboardPage");
      }
   }
 };
