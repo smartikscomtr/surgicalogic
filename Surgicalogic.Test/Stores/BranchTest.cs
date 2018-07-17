f33668de16eb0bf009bf2f42bafeb14a1b47df12
Hasan Çolak <hasan.colak@smartiks.com.tr>
2018-07-17 13:30:32 +0300

    Launch fix

---
+++ Surgicalogic.Test/Stores/BranchTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+namespace Surgicalogic.Test.Stores
+{
+    using Surgicalogic.Contracts.Stores;
+    using Surgicalogic.Model.CommonModel;
+    using Surgicalogic.Model.OutputModel;
+    using Surgicalogic.Services.Stores;
+    using System.Threading.Tasks;
+           
+    public class BranchTest
+    {
+           
+        [Fact]
+        public async Task GetBranch()
+        {
+           
+        }
+    }
+}
