9c840fcd4871d466c4c6ecef93995c072366e767
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2018-07-18 16:38:08 +0300

    grid component fixed.

--- Surgicalogic.Web/src/components/GridComponent.vue
+++ Surgicalogic.Web/src/components/GridComponent.vue
@@ -8,12 +8,19 @@
 
         <v-container>
           <v-card-title>
-            <v-text-field v-model="search" append-icon="search" label="Search" v-on:keyup.enter="filterGrid" single-line hide-details>
+            <v-text-field v-model="search"
+                          append-icon="search"
+                          label="Search"
+                          v-on:keyup.enter="filterGrid"
+                          single-line
+                          hide-details>
             </v-text-field>
 
             <v-spacer></v-spacer>
 
-            <v-btn class="orange" slot="activator" @click="addNewItem">
+            <v-btn class="orange"
+                   slot="activator"
+                   @click="addNewItem">
               <v-icon color="white--text">
                 add
               </v-icon>
@@ -21,7 +28,11 @@
             </v-btn>
           </v-card-title>
 
-          <v-data-table :headers="headers" :items="items" :pagination.sync="pagination" :total-items="totalCount" :rows-per-page-items="[10, 20, { 'text': $t('common.all'), 'value': -1 }]">
+          <v-data-table :headers="headers"
+                        :items="items"
+                        :pagination.sync="pagination"
+                        :total-items="totalCount"
+                        :rows-per-page-items="[10, 20, { 'text': $t('common.all'), 'value': -1 }]">
             <template slot="items" slot-scope="props">
               <td v-for="(header, i) in headers" :key="i">
                 <template v-if="!header.isAction">
@@ -29,19 +40,27 @@
                 </template>
 
                 <template v-else>
-                  <v-btn v-if="showDetail" icon class="mx-0" @click="detailItem(props.item)">
+                  <v-btn v-if="showDetail"
+                         icon
+                         class="mx-0"
+                         @click="detailItem(props.item)">
                     <v-icon color="#232222">
                       visibility
                     </v-icon>
                   </v-btn>
 
-                  <v-btn v-if="showEdit" icon class="mx-0" @click="editItem(props.item)">
+                  <v-btn v-if="showEdit"
+                         icon class="mx-0"
+                         @click="editItem(props.item)">
                     <v-icon color="#232222">
                       edit
                     </v-icon>
                   </v-btn>
 
-                  <v-btn v-if="showDelete" icon class="mx-0" @click="deleteItem(props.item)">
+                  <v-btn v-if="showDelete"
+                         icon
+                         class="mx-0"
+                         @click="deleteItem(props.item)">
                     <v-icon color="#232222">
                       delete
                     </v-icon>
@@ -57,127 +76,133 @@
 </template>
 
 <script>
+
 export default {
-    props: {
-        items: {
-            type: Array,
-            required: false,
-            default() {
-                return [];
-            }
-        },
-        totalCount: {
-            type: Number,
-            required: false
-        },
-
-        headers: {
-            type: Array,
-            required: false
-        },
-
-        title: {
-            type: String,
-            required: false
-        },
-
-        showDetail: {
-            type: Boolean,
-            required: false
-        },
-
-        showEdit: {
-            type: Boolean,
-            required: false
-        },
-
-        showDelete: {
-            type: Boolean,
-            required: false
-        },
-
-        methodName: {
-            type: Function,
-            required: true
-        }
+  props: {
+    items: {
+      type: Array,
+      required: false,
+      default() {
+          return [];
+      }
+    },
+    totalCount: {
+      type: Number,
+      required: false
     },
 
-    data() {
-        return {
-            search: '',
-            loading: true,
-            dataRows: [],
-            pagination: {},
-            pagingExecuted: false
-        };
+    headers: {
+      type: Array,
+      required: false
     },
 
-    watch: {
-        pagination: {
-            handler(newValue, oldValue) {
-                const vm = this;
-
-                if (!oldValue.rowsPerPage || (oldValue.rowsPerPage && vm.pagingExecuted))
-                {
-                  vm.executeGridOperations();
-                }
-                else
-                {
-                  vm.pagingExecuted = true;
-                }
-            }
-        }
+    title: {
+      type: String,
+      required: false
     },
 
-    methods: {
-        addNewItem() {
-            const vm = this;
+    showDetail: {
+      type: Boolean,
+      required: false
+    },
 
-            //When the add new button is clicked, the event is sent to the grid component
-            vm.$emit('newaction');
-        },
+    showEdit: {
+      type: Boolean,
+      required: false
+    },
 
-        filterGrid() {
-            const vm = this;
+    showDelete: {
+      type: Boolean,
+      required: false
+    },
 
-            vm.executeGridOperations();
-        },
-
-        detailItem(item) {
-            const vm = this;
-
-            //When the detail button is clicked, the event is sent to the grid component
-            vm.$emit('detail', item);
-        },
-
-        editItem(item) {
-            const vm = this;
-
-            //When the edit button is clicked, the event is sent to the grid component
-            vm.$emit('edit', item);
-        },
-
-        deleteItem(item) {
-            const vm = this;
-
-            //When the delete button is clicked, the event is sent to the grid component
-            vm.$emit('deleteitem', item);
-        },
-
-        executeGridOperations() {
-            const vm = this;
-
-            const { sortBy, descending, page, rowsPerPage } = vm.pagination;
-
-            vm.$store.dispatch(vm.methodName(), {
-                currentPage: page,
-                pageSize: rowsPerPage,
-                search: vm.search,
-                sortBy: sortBy,
-                descending: descending
-            });
-        }
+    methodName: {
+      type: Function,
+      required: true
     }
+  },
+
+  data() {
+    return {
+      search: '',
+      loading: true,
+      dataRows: [],
+      pagination: {},
+      pagingExecuted: false
+    };
+  },
+
+  watch: {
+    pagination: {
+      handler(newValue, oldValue) {
+        const vm = this;
+
+        if (!oldValue.rowsPerPage || (oldValue.rowsPerPage && vm.pagingExecuted))
+        {
+          vm.executeGridOperations();
+        }
+        else
+        {
+          vm.pagingExecuted = true;
+        }
+      }
+    }
+  },
+
+  methods: {
+    addNewItem() {
+      const vm = this;
+
+      //When the add new button is clicked, the event is sent to the grid component
+      vm.$emit('newaction');
+    },
+
+    filterGrid() {
+      const vm = this;
+
+      vm.executeGridOperations();
+    },
+
+    detailItem(item) {
+      const vm = this;
+
+      //When the detail button is clicked, the event is sent to the grid component
+      vm.$emit('detail', item);
+    },
+
+    editItem(item) {
+      const vm = this;
+
+      //When the edit button is clicked, the event is sent to the grid component
+      vm.$emit('edit', item);
+    },
+
+    deleteItem(item) {
+      const vm = this;
+
+      //When the delete button is clicked, the event is sent to the grid component
+      vm.$emit('deleteitem', item);
+    },
+
+    executeGridOperations() {
+      const vm = this;
+
+      const {
+        sortBy,
+        descending,
+        page,
+        rowsPerPage
+      } = vm.pagination;
+
+      vm.$store.dispatch(vm.methodName(), {
+        currentPage: page,
+        pageSize: rowsPerPage,
+        search: vm.search,
+        sortBy: sortBy,
+        descending: descending
+      });
+    }
+  }
 };
 </script>
 
