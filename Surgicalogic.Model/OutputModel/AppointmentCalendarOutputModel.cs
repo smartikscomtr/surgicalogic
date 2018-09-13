8dffa7b19705773dd00bee9ee6c1c9a74e5e9a73
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2018-09-13 16:22:46 +0300

    api fixes

---
+++ Surgicalogic.Model/OutputModel/AppointmentCalendarOutputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surgicalogic.Model.OutputModel
+{
+    public class AppointmentCalendarOutputModel
+    {
+        public int Id { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int PersonnelId { get; set; }
+        public int PatientId { get; set; }
+    }
+}
