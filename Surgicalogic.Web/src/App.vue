feb4e14fe71e460608913c4ef4ff0b5edb795c91
gurkank <gurkank@GURKANKESEBIR>
2018-10-05 11:47:57 +0300

    ünvan ikonu değiştirildi.

--- Surgicalogic.Web/src/App.vue
+++ Surgicalogic.Web/src/App.vue
@@ -262,7 +262,7 @@ export default {
               route: "/PersonnelCategorypage"
             },
             {
-              icon: "person_pin",
+              icon: "grade",
               text: vm.$i18n.t("menu.PersonnelTitles"),
               route: "/PersonnelTitlepage"
             },
