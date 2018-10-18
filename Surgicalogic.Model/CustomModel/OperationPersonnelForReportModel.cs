979ecaca341683e30573a4dff9b11f823a75c924
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2018-10-18 09:36:16 +0300

    Rapor modellemeleri yapıldı.

---
+++ Surgicalogic.Model/CustomModel/OperationPersonnelForReportModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surgicalogic.Model.CustomModel
+{
+    public class OperationPersonnelForReportModel : EntityModel.Base.EntityModel
+    {
+        public PersonnelForReportModel Personnel { get; set; }
+    }
+}
