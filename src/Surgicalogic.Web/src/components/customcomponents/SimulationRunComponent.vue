f12eff3a992eadb564c11072eefd14beb18e5c6a
Tuba Bayraktutar <tuba.bayraktutar@smartiks.com.tr>
2019-02-20 09:39:53 +0300

    Simulation frontend geliştirmesi

--- src/Surgicalogic.Web/src/components/customcomponents/SimulationRunComponent.vue
+++ src/Surgicalogic.Web/src/components/customcomponents/SimulationRunComponent.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    <div class="container fluid grid-list-md">
-      <div class="grid-card">
+    <div class="v-card__title">
+      <v-flex xs12 sm6 md3>
+        <v-menu ref="menu"
+                :close-on-content-click="false"
+                v-model="menu"
+                :nudge-right="40"
+                :return-value.sync="date"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                min-width="290px">
+          <v-text-field append-icon="keyboard_arrow_down"
+                        readonly
+                        slot="activator"
+                        clearable
+                        v-model="dateFormatted"
+                        :label="$t('operation.operationDate')">
+          </v-text-field>
+
+          <v-date-picker v-model="date"
+                          no-title
+                          @input="$refs.menu.save(date)">
+          </v-date-picker>
+        </v-menu>
+      </v-flex>
+
+      <v-flex xs12 sm6 md3>
         <v-btn class="orange"
-               @click="runSimulation">
+              @click="runSimulation">
           <span lang="tr">{{ $t('simulation.simulate') }}</span>
         </v-btn>
-      </div>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+      </v-flex>
     </div>
 
     <div v-if="simulation">
@@ -19,6 +48,7 @@
                       :show-search="false"
                       :show-insert="false"
                       :hide-actions="false"
+                      :hide-export="true"
                       :methodName="getMethodName"
                       :loading="getLoading"
                       :totalCount="getTotalCount"
@@ -44,6 +74,9 @@ export default {
     const vm = this;
 
     return {
+      menu: false,
+      dateFormatted: null,
+      date: null,
       search: '',
       detailDialog: false,
       editDialog: false,
@@ -57,6 +90,14 @@ export default {
       deletePath: '',
       simulation: false
     };
+  },
+
+  watch: {
+    date(val) {
+      const vm = this;
+
+      vm.dateFormatted = vm.formatDate(vm.date);
+    }
   },
 
   computed: {
@@ -79,15 +120,15 @@ export default {
           align: 'left'
         },
         {
-          value: 'usage',
-          sortBy: 'usage',
-          text: vm.$i18n.t('simulation.usage'),
+          value:'overTime',
+          text: vm.$i18n.t('simulation.overtime'),
           sortable: true,
           align: 'left'
         },
         {
-          value:'overTime',
-          text: vm.$i18n.t('simulation.overtime'),
+          value: 'usage',
+          sortBy: 'usage',
+          text: vm.$i18n.t('simulation.usage'),
           sortable: true,
           align: 'left'
         },
@@ -108,7 +149,7 @@ export default {
     simulations() {
       const vm = this;
 
-      return vm.$store.state.simulationModule.tomorrowList;
+      return vm.$store.state.simulationModule.operationList;
     },
 
     getLoading() {
@@ -120,13 +161,15 @@ export default {
     getTotalCount() {
       const vm = this;
 
-      return vm.$store.state.simulationModule.tomorrowListTotalCount;
+      return vm.$store.state.simulationModule.operationListTotalCount;
     }
   },
 
   methods: {
     getMethodName(){
-      return "runSimulation";
+      const vm = this;
+
+      return `${"runSimulation"}, ${vm.date}`;
     },
 
     deleteMethodName(){
@@ -136,13 +179,35 @@ export default {
     runSimulation() {
       const vm = this;
 
+      vm.$store.dispatch('runSimulation', {
+        selectDate: vm.date
+      });
       vm.simulation = true;
+    },
+
+    getDate() {
+      const toTwoDigits = num => (num < 10 ? "0" + num : num);
+      let today = new Date();
+      let year = today.getFullYear();
+      let month = toTwoDigits(today.getMonth() + 1);
+      let day = toTwoDigits(today.getDate() + 1);
+
+      return `${year}-${month}-${day}`;
+    },
+
+    formatDate(date) {
+      if (!date || date.indexOf(".") > -1) return null;
+
+      const [year, month, day] = date.split("-");
+
+      return `${day}.${month}.${year}`;
     }
   },
 
   created () {
     const vm = this;
 
+    vm.date = vm.getDate();
     vm.$store.dispatch('getAllOperatingRooms');
   }
 };
